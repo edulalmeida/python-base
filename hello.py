f3d7fa48ca53ffd40d7634eab6b5e67ccdb11e69
Edu Almeida <edulalmeida@gmail.com>
2025-05-20 22:08:08 -0300

    Adicionado versao Hello Worls multi linguage

--- hello.py
+++ hello.py
@@ -1,7 +1,48 @@
-#!/usr/bin/env python3
+#/usr/bin/env/ python3
 
-# Este programa imprime Hello World
 
-print("Hello, World!")
+
+
+
+"""
+Hello World Multi Linguas
+
+Dependeno da lingua configurada no ambiente o programa exibe a correspondente.
+
+Como usar:
+Tenha a variavel LANG devidademente configurada, por ex:
+    export LANG=pt_BR
+
+Execução:
+    python3 hello.py
+    ou 
+    ./hello.py
+
+"""
+__version__ = "0.0.1"
+__author__ = "Edu Almeida"
+__license__ = "Unlicense"
+
+# Dunder = __(Underline Underline)
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Boujour Monde"
+
+print(msg)
+
+
+
 
 
